abd79c0e8a2d81b3deae030e857783ca46762e73
parzivail <colby.newman2000@gmail.com>
2022-07-31 19:25:33 -0400

    move gatt characteristic to lib

--- BleBeacon/BleDevice.cs
+++ BleBeacon/BleDevice.cs
@@ -41,9 +41,12 @@ internal class SampleGattApplication
             Primary = true
         };
 
+        var c = new ValueBackedCharacteristicSource();
+        c.ValueSet += (value, response, didNotify) => { Console.WriteLine(Encoding.UTF8.GetString(value)); };
+
         var gattCharacteristicDescription = new GattCharacteristicDescription
         {
-            CharacteristicSource = new ExampleCharacteristicSource(),
+            CharacteristicSource = c,
             UUID = "12345678-1234-5678-1234-56789abcdef1",
             Flags = CharacteristicFlags.Read | CharacteristicFlags.Write | CharacteristicFlags.WritableAuxiliaries |
                     CharacteristicFlags.Notify
@@ -61,78 +64,6 @@ internal class SampleGattApplication
 
         await new GattApplicationManager(serverContext)
             .RegisterGattApplication("/org/bluez/example/gattapp0", gab.BuildServiceDescriptions());
-    }
-
-    internal class ExampleCharacteristicSource : ICharacteristicSource
-    {
-        const bool NotificationAvoiding = false;
-
-        public override Task WriteValueAsync(byte[] value, bool response)
-        {
-            // The client sent us some data!
-
-            Console.WriteLine(Encoding.ASCII.GetChars(value));
-
-            if (NotificationAvoiding)
-            {
-                // We get our properties, so that we can directly write the value
-                var props = Properties.GetAllAsync().Result;
-
-                Console.WriteLine("Writing value");
-                // Assign the value directly, since we don't want to notify the client of what it sent us - it should probably know that!
-                props.Value = value;
-                return Task.CompletedTask;
-            }
-
-            Console.WriteLine("Writing value");
-            // Let's write it to our characteristic's value! (This will trigger a notification, if they're turned on)
-            return Properties.SetAsync("Value", value);
-        }
-
-        public override Task<byte[]> ReadValueAsync()
-        {
-            // The client asked for our value
-            Console.WriteLine("Reading value");
-
-            // Get our value, in the form of a byte array task
-            var props = Properties.GetAsync<byte[]>("Value");
-
-            // Return our task, mission accomplished!
-            return props;
-        }
-
-        public override Task StartNotifyAsync()
-        {
-            // Our client has requested notifications!
-            Console.WriteLine("Starting to Notify");
-
-            // Get our properties class
-            var props = Properties.GetAllAsync().Result;
-            // Set notifying to true, so we'll start sending replies on Properties.SetAsync("Value", [value])
-            props.Notifying = true;
-            // Return something, since we have to.
-            return Task.CompletedTask;
-        }
-
-        public override Task StopNotifyAsync()
-        {
-            // Our client has asked us to stop talking to it :(
-            Console.WriteLine("Stopping notifications..");
-
-            // Get our properties class again
-            var props = Properties.GetAllAsync().Result;
-            // Set notifying to false. Now Properties.SetAsync("Value", [value]) won't send notifications anymore.
-            props.Notifying = false;
-            // Return a completed task so we can leave.
-            return Task.CompletedTask;
-        }
-
-        public override Task ConfirmAsync()
-        {
-            // The client told us that it got the packet. That's very polite!
-            // Indicate characteristics will have their notifications confirmed by the client, which will trigger this function.
-            return Task.CompletedTask;
-        }
     }
 }
 
