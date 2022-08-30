74e9fabe1324a3fb01471096142fb6d1c491f9de
parzivail <colby.newman2000@gmail.com>
2022-08-30 12:15:10 -0400

    basic image output

--- Unpacker/Program.cs
+++ Unpacker/Program.cs
@@ -21,6 +21,8 @@ public record TimestampedObject<T>(DateTime Timestamp, T Data) : IComparable<Tim
     }
 }
 
+public record GpsAndTimestampedObject<T>(DateTime Timestamp, GpsPosition Position, T Data) : TimestampedObject<T>(Timestamp, Data);
+
 public record GpsPosition(double Latitude, double Longitude, int SatelliteCount, TimeOnly UtcTime);
 
 public class GpsInterpolator
@@ -40,7 +42,8 @@ public class GpsInterpolator
         if (time > Data[^1].Timestamp)
             return Data[^1].Data;
 
-        var index = Array.BinarySearch(Data, new TimestampedObject<GpsPosition>(time, new GpsPosition(0, 0, 0, TimeOnly.MinValue)));
+        var index = Array.BinarySearch(Data,
+            new TimestampedObject<GpsPosition>(time, new GpsPosition(0, 0, 0, TimeOnly.MinValue)));
         if (index >= 0)
             return Data[index].Data;
 
@@ -117,47 +120,40 @@ public record MapObjectDataEntry(
 
 public class Program
 {
-    public static void Main(string[] args)
+    private const double OSM_T = 28.35546;
+    private const double OSM_B = 28.35309;
+    private const double OSM_L = -81.56299;
+    private const double OSM_R = -81.55967;
+
+    private const double WDW_T = 28.35570;
+    private const double WDW_B = 28.35334;
+    private const double WDW_L = -81.56279;
+    private const double WDW_R = -81.55983;
+
+    private static double Remap(double t, double a, double b, double x, double y)
     {
-        var beaconData = JsonSerializer.Deserialize<Dictionary<string, BeaconDataEntry>>(
-            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_beacon-data.json")
-        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
-        var mapLocationData = JsonSerializer.Deserialize<Dictionary<string, MapLocationDataEntry>>(
-            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_map-location-data-wdw.json")
-        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
-        var mapObjectData = JsonSerializer.Deserialize<Dictionary<string, MapObjectDataEntry>>(
-            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_map-object-data-wdw.json")
-        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
-
-        var map = Image.Load<Rgba32>("/home/cnewman/Documents/map-wdw.png");
-
-        var collection = new FontCollection();
-        var family = collection.Add("/home/cnewman/.fonts/iosevka-p-term/iosevka-p-term-regular.ttf");
-        var font = family.CreateFont(16);
-        
-        foreach (var (id, entry) in mapObjectData.Where(pair => pair.Value.Type == "BeaconMarker"))
-        {
-            var pos = mapLocationData[entry.LocationId];
-            var data = beaconData[entry.BeaconId];
-
-            var x = (float)(pos.X * map.Width);
-            var y = (float)(pos.Y * map.Height);
-
-            var pt = new PointF(x, y);
-            
-            map.Mutate(context =>
-            {
-                context.Fill(Color.Red, new EllipsePolygon(pt, 3));
-                context.DrawText($"{data.Name}\n{data.WaypointId} 0x{data.WaypointId:X}", font, Color.White, pt);
-            });
-        }
-        
-        map.Save("/home/cnewman/Documents/map-wdw-beacons.png");
+        return (t - a) / (b - a) * (y - x) + x;
     }
 
-    public static void MainCreateCsv(string[] args)
+    private static (double X, double Y) WdwMapPointToOsmMapPoint(double x, double y)
     {
-        using var gpsStream = File.OpenRead("/home/cnewman/Documents/SWGE Data 8-22-22/gps_637956226262456597.bin");
+        var wdwLon = Remap(x, 0, 1, WDW_L, WDW_R);
+        var wdwLat = Remap(y, 0, 1, WDW_T, WDW_B);
+        return LatLonToOsmMapPoint(wdwLon, wdwLat);
+    }
+
+    private static (double X, double Y) LatLonToOsmMapPoint(double wdwLon, double wdwLat)
+    {
+        return (
+            Remap(wdwLon, OSM_L, OSM_R, 0, 1),
+            Remap(wdwLat, OSM_T, OSM_B, 0, 1)
+        );
+    }
+
+    public static void Main(string[] args)
+    {
+        // Load GPS data
+        using var gpsStream = File.OpenRead("/home/cnewman/Documents/gps_637956226262456597.bin");
         var gps = new BinaryReader(gpsStream);
         var gpsPings = new List<TimestampedObject<GpsPosition>>();
 
@@ -177,17 +173,21 @@ public class Program
             // ignored
         }
 
+        // Synchronize GPS and system time
         var trueDate = new DateOnly(2022, 8, 22);
         var trueTime = gpsPings[0].Data.UtcTime;
         var trueTimestamp = trueDate.ToDateTime(trueTime, DateTimeKind.Utc);
         var localTimestamp = gpsPings[0].Timestamp;
         var timeOffset = trueTimestamp - localTimestamp;
 
-        var gpsInterpolator = new GpsInterpolator(gpsPings.Select(o => o with { Timestamp = o.Timestamp + timeOffset }).ToArray());
+        var gpsInterpolator =
+            new GpsInterpolator(gpsPings.Select(o => o with { Timestamp = o.Timestamp + timeOffset }).ToArray());
 
-        using var bleStream = File.OpenRead("/home/cnewman/Documents/SWGE Data 8-22-22/ble_sniffer_637956226262456597.bin");
+        // Load BLE data
+        using var bleStream =
+            File.OpenRead("/home/cnewman/Documents/ble_sniffer_637956226262456597.bin");
         var ble = new BinaryReader(bleStream);
-        var packets = new List<TimestampedObject<byte[]>>();
+        var rawPackets = new List<TimestampedObject<byte[]>>();
 
         try
         {
@@ -197,7 +197,7 @@ public class Program
                 var packetLength = ble.ReadInt32();
                 var data = ble.ReadBytes(packetLength);
 
-                packets.Add(new TimestampedObject<byte[]>(timestamp, data));
+                rawPackets.Add(new TimestampedObject<byte[]>(timestamp, data));
             }
         }
         catch (EndOfStreamException)
@@ -205,13 +205,11 @@ public class Program
             // ignored
         }
 
+        // Create PCAP-compatible packets
+        var packets = new List<GpsAndTimestampedObject<byte[]>>();
+
         var manager = new RecyclableMemoryStreamManager();
-
-        var sw = new StreamWriter("/mnt/x/SWGE/packets.csv");
-
-        sw.WriteLine("Latitude,Longitude,SatelliteCount,Device,RSSI");
-
-        foreach (var (time, data) in packets)
+        foreach (var (time, data) in rawPackets)
         {
             if (data.Length <= 35)
                 continue;
@@ -234,6 +232,44 @@ public class Program
             if ((flags & flagCrcOk) == 0)
                 continue;
 
+            packets.Add(new GpsAndTimestampedObject<byte[]>(time, gpsLocation, packet));
+        }
+
+        // Load Datapad beacon definitions
+        var beaconData = JsonSerializer.Deserialize<Dictionary<string, BeaconDataEntry>>(
+            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_beacon-data.json")
+        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
+        var mapLocationData = JsonSerializer.Deserialize<Dictionary<string, MapLocationDataEntry>>(
+            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_map-location-data-wdw.json")
+        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
+        var mapObjectData = JsonSerializer.Deserialize<Dictionary<string, MapObjectDataEntry>>(
+            File.ReadAllText("/home/cnewman/Documents/starWarsGalaxysEdgeGame_91_map-object-data-wdw.json")
+        ).Select(pair => pair.Value).ToDictionary(entry => entry.Id, entry => entry);
+
+        // Load images
+        // var wdwMap = Image.Load<Rgba32>("/home/cnewman/Documents/map-wdw.png");
+        var osmMap = Image.Load<Rgba32>("/home/cnewman/Documents/osm_map_no_text.png");
+
+        // Setup fonts
+        var collection = new FontCollection();
+        var family = collection.Add("/home/cnewman/.fonts/iosevka-p-term/iosevka-p-term-regular.ttf");
+        var font = family.CreateFont(12);
+
+        const int targetBeacon = 0xA5;
+
+        var beaconPos = new Dictionary<int, (double X, double Y)>();
+        foreach (var (id, entry) in mapObjectData.Where(pair => pair.Value.Type == "BeaconMarker"))
+        {
+            var pos = mapLocationData[entry.LocationId];
+            var data = beaconData[entry.BeaconId];
+            beaconPos[data.WaypointId] = new ValueTuple<double, double>(pos.X, pos.Y);
+        }
+
+        foreach (var (time, pos, packet) in packets)
+        {
+            if (pos.SatelliteCount < 10)
+                continue;
+
             var rssi = -packet[10];
             var macAddr = new PhysicalAddress(new[]
             {
@@ -249,8 +285,50 @@ public class Program
             if (adManufacturer != 0x183) // Walt Disney
                 continue;
 
-            sw.WriteLine($"{gpsLocation.Latitude},{gpsLocation.Longitude},{gpsLocation.SatelliteCount},{macAddr},{rssi}");
+            var advertType = (packet[36] << 8) | packet[37];
+            if (advertType != 0x1004) // Beacon
+                continue;
+
+            var beaconId = packet[40];
+
+            // if (beaconId != targetBeacon)
+            //     continue;
+
+            var (x, y) = LatLonToOsmMapPoint(pos.Longitude, pos.Latitude);
+            var pt = new PointF((float)(x * osmMap.Width), (float)(y * osmMap.Height));
+            osmMap.Mutate(context =>
+            {
+                if (beaconPos.TryGetValue(beaconId, out var entry))
+                {
+                    var (sx, sy) = WdwMapPointToOsmMapPoint(entry.X, entry.Y);
+                    var spt = new PointF((float)(sx * osmMap.Width), (float)(sy * osmMap.Height));
+
+                    context.DrawLines(Color.Black, 1, pt, spt);
+                }
+
+                context.Fill(Color.Blue, new EllipsePolygon(pt, 3));
+                // context.DrawText($"{beaconId} 0x{beaconId:X}", font, Color.Black, pt);
+            });
         }
+
+        // Draw Datapad beacons
+        foreach (var (id, entry) in mapObjectData.Where(pair => pair.Value.Type == "BeaconMarker"))
+        {
+            var pos = mapLocationData[entry.LocationId];
+            var data = beaconData[entry.BeaconId];
+
+            var (x, y) = WdwMapPointToOsmMapPoint(pos.X, pos.Y);
+            var pt = new PointF((float)(x * osmMap.Width), (float)(y * osmMap.Height));
+
+            osmMap.Mutate(context =>
+            {
+                var bWaypoint = (byte)data.WaypointId;
+                context.Fill(Color.Red, new EllipsePolygon(pt, 3));
+                context.DrawText($"{data.Name}\n{data.WaypointId} 0x{data.WaypointId:X}", font, Color.Black, pt);
+            });
+        }
+
+        osmMap.Save("/home/cnewman/Documents/osm_map_beacons.png");
     }
 
     private static GpsPosition? ParseSentence(string nmeaSentence)
@@ -288,7 +366,8 @@ public class Program
         var degLatitude = latM * (latitudeDeg + latitudeMinutes / 60);
         var degLongitude = lonM * (longitudeDeg + longitudeMinutes / 60);
 
-        return new GpsPosition(degLatitude, degLongitude, numSatellites, new TimeOnly(utcHours, utcMinutes, (int)utcSeconds, (int)(utcSeconds * 1000) % 1000));
+        return new GpsPosition(degLatitude, degLongitude, numSatellites,
+            new TimeOnly(utcHours, utcMinutes, (int)utcSeconds, (int)(utcSeconds * 1000) % 1000));
     }
 
     public static void MainBLE(string[] args)
